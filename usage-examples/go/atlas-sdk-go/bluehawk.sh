0f380c59d0c0882766d56dadd069356848d3d375
cbullinger <cory.bullinger@mongodb.com>
2025-03-20 10:09:00 -0400

    Cleaning up generated-usage-examples directory

--- usage-examples/go/atlas-sdk-go/bluehawk.sh
+++ usage-examples/go/atlas-sdk-go/bluehawk.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 PROJECT=$(git rev-parse --show-toplevel)
-GO_SDK_EXAMPLES=$PROJECT/usage-examples/go/atlas-sdk-go/cmd
-GENERATED_EXAMPLES=$PROJECT/generated-usage-examples/go/sdk
+GO_SDK_EXAMPLES=$PROJECT/usage-examples/go/atlas-sdk-go/
+GENERATED_EXAMPLES=$PROJECT/generated-usage-examples/go/atlas-sdk-go/
 
 # Bluehawk Admin Go SDK examples
 echo "Running Bluehawk snip on $GO_SDK_EXAMPLES"
 echo "Extracting snippets to the $GENERATED_EXAMPLES directory"
 npx bluehawk snip "$GO_SDK_EXAMPLES" -o "$GENERATED_EXAMPLES"
 
-# Bluehawk with state tags
+# Bluehawk with state tag
 echo "Running Bluehawk snip on state tags"
 npx bluehawk snip "$GO_SDK_EXAMPLES" -o "$GENERATED_EXAMPLES" --state dev
 npx bluehawk snip "$GO_SDK_EXAMPLES" -o "$GENERATED_EXAMPLES" --state prod
